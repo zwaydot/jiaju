c058d4acb966f7d7892095446f55564f01872bb6
zway <zwaydot@gmail.com>
2023-05-12 12:38:37 +0800

    create Cloudflare Worker

--- scripts.js
+++ scripts.js
@@ -1,31 +1,35 @@
+// Fetch data from Notion through Cloudflare Worker
 async function fetchNotionData() {
-  const databaseId = "222230cd10754cddb5745639ed429211"; // 你的 Notion 数据库 ID
-  const response = await fetch(`https://api.notion.com/v1/databases/${databaseId}/query`, {
-      method: "POST",
+  const endpoint = "https://crimson-shape-242d.zwaydot4972.workers.dev/";
+  let response = await fetch(endpoint, {
+      method: 'POST',
       headers: {
-          "Authorization": `Bearer secret_jE9mwRlSP3PRAkhganBnVpxVeHV8r8ALlFGlLtVxHPy`, // 你的 Notion Integration Token
-          "Notion-Version": "2021-08-16"
+          'Content-Type': 'application/json'
       },
-      body: JSON.stringify({ page_size: 100 }) // 设置获取的数据量，你可以根据需要调整
+      body: JSON.stringify({
+          "filter": {
+              "property": "Active",
+              "checkbox": {
+                  "equals": true
+              }
+          }
+      })
   });
   
-  const data = await response.json();
-  return data.results; // 返回结果中的数据部分
+  if (!response.ok) {
+      throw new Error("Notion API error: " + response.status);
+  }
+  
+  let data = await response.json();
+  return data.results;
 }
 
-function createBrandLink(brand) {
-  const brandLink = document.createElement('a');
-  brandLink.href = brand.url;
-  brandLink.textContent = brand.name;
-  return brandLink;
-}
-
+// Load brand data when the window is loaded
 window.onload = async function() {
-  const brands = await fetchNotionData();
-  const brandList = document.getElementById('brand-list');
-
-  for (let brand of brands) {
-      const brandLink = createBrandLink(brand);
-      brandList.appendChild(brandLink);
+  try {
+      let brandData = await fetchNotionData();
+      // Process the data as needed, e.g., display it on the webpage
+  } catch (error) {
+      console.error("Failed to fetch brand data: " + error.message);
   }
 };
