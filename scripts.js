aa4f2786d3d286553431d47076380bfc038e1e1f
zway <zwaydot@gmail.com>
2023-05-12 12:21:32 +0800

    change the notion api and the background color

--- scripts.js
+++ scripts.js
@@ -1,44 +1,31 @@
-const NOTION_API_KEY = 'secret_jE9mwRlSP3PRAkhganBnVpxVeHV8r8ALlFGlLtVxHPy'; // 请替换为你的 API 密钥
-const DATABASE_ID = '222230cd10754cddb5745639ed429211'; // 请替换为你的数据库 ID
-
 async function fetchNotionData() {
-  const url = `https://api.notion.com/v1/databases/${DATABASE_ID}/query`;
-
-  const requestOptions = {
-    method: 'POST',
-    headers: {
-      'Authorization': `Bearer ${NOTION_API_KEY}`,
-      'Content-Type': 'application/json',
-      'Notion-Version': '2022-05-13', // 请使用 Notion API 的最新版本
-    },
-  };
-
-  try {
-    const response = await fetch(url, requestOptions);
-    const data = await response.json();
-    displayBrands(data);
-  } catch (error) {
-    console.error('获取 Notion 数据失败:', error);
-  }
-}
-
-function displayBrands(data) {
-  const brandContainer = document.getElementById('brand-container');
-
-  data.results.forEach((brand) => {
-    const brandName = brand.properties.Name.title[0].text.content;
-    const brandUrl = brand.properties.URL.url;
-
-    const brandElement = document.createElement('div');
-    brandElement.classList.add('brand');
-
-    const linkElement = document.createElement('a');
-    linkElement.href = brandUrl;
-    linkElement.innerText = brandName;
-
-    brandElement.appendChild(linkElement);
-    brandContainer.appendChild(brandElement);
+  const databaseId = "222230cd10754cddb5745639ed429211"; // 你的 Notion 数据库 ID
+  const response = await fetch(`https://api.notion.com/v1/databases/${databaseId}/query`, {
+      method: "POST",
+      headers: {
+          "Authorization": `Bearer secret_jE9mwRlSP3PRAkhganBnVpxVeHV8r8ALlFGlLtVxHPy`, // 你的 Notion Integration Token
+          "Notion-Version": "2021-08-16"
+      },
+      body: JSON.stringify({ page_size: 100 }) // 设置获取的数据量，你可以根据需要调整
   });
+  
+  const data = await response.json();
+  return data.results; // 返回结果中的数据部分
 }
 
-fetchNotionData();
+function createBrandLink(brand) {
+  const brandLink = document.createElement('a');
+  brandLink.href = brand.url;
+  brandLink.textContent = brand.name;
+  return brandLink;
+}
+
+window.onload = async function() {
+  const brands = await fetchNotionData();
+  const brandList = document.getElementById('brand-list');
+
+  for (let brand of brands) {
+      const brandLink = createBrandLink(brand);
+      brandList.appendChild(brandLink);
+  }
+};
