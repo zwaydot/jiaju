febed83aa5115ded45d42baff91dff76d3ab88f4
zway <zwaydot@yeah.net>
2023-05-13 23:31:18 +0800

    logo bug fix

--- scripts.js
+++ scripts.js
@@ -30,9 +30,18 @@ window.onload = async () => {
             intro.textContent = item.properties.Introduction?.rich_text[0]?.plain_text || 'No Introduction';
             brandElement.appendChild(intro);
 
+            // const logo = document.createElement('img');
+            // logo.src = item.properties.Logo?.url || 'No Logo';
+            // brandElement.appendChild(logo);
+
             const logo = document.createElement('img');
-            logo.src = item.properties.Logo?.url || 'No Logo';
+            if (item.properties.Logo && item.properties.Logo.files && item.properties.Logo.files.length > 0) {
+                logo.src = item.properties.Logo.files[0].url;
+            } else {
+                logo.src = 'https://scontent-xsp1-3.cdninstagram.com/v/t51.2885-19/20590116_694938284040669_1940382356506411008_a.jpg?stp=dst-jpg_s320x320&_nc_ht=scontent-xsp1-3.cdninstagram.com&_nc_cat=109&_nc_ohc=t5YVPtl73q8AX8CicZd&edm=AOQ1c0wBAAAA&ccb=7-5&oh=00_AfAOdmFRuRXTUeQv1lVOMgyTkvjwAplp-zNt3Yb_Tzii5g&oe=64650251&_nc_sid=8fd12b';  // 请替换为你的默认Logo URL
+            }
             brandElement.appendChild(logo);
+
 
             const url = document.createElement('a');
             url.href = item.properties.URL?.url || '#';
