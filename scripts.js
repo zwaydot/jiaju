e53bdfbfb8689dd69c3401816d23c913976ed862
zway <zwaydot@gmail.com>
2023-05-15 14:56:45 +0800

    bug fix

--- scripts.js
+++ scripts.js
@@ -13,76 +13,72 @@ const fetchNotionData = async () => {
     }
 
     const data = await response.json();
-
-    // 在这里插入console.log(data.results);
     console.log(data.results);
-
     return data;
+};
+
+const createBrandElement = (item) => {
+    const brandElement = document.createElement('div');
+    brandElement.classList.add('brand-card');
+
+    const brandName = document.createElement('h2');
+    brandName.textContent = item.properties.Brand?.title[0]?.plain_text || 'Unknown';
+    brandElement.appendChild(brandName);
+
+    const intro = document.createElement('p');
+    intro.textContent = item.properties.Introduction?.rich_text[0]?.plain_text || 'No Introduction';
+    brandElement.appendChild(intro);
+
+    const logoFile = item.properties.Logo?.files[0];
+    if (logoFile && logoFile.file) {
+        const logo = document.createElement('img');
+        logo.src = logoFile.file.url; 
+        brandElement.appendChild(logo);
+    } else {
+        console.warn('No Logo for item:', item);
+    }
+
+    const pictureFile = item.properties.Picture?.files[0];
+    if (pictureFile && pictureFile.file) {
+        const picture = document.createElement('img');
+        picture.src = pictureFile.file.url;
+        brandElement.appendChild(picture);
+    } else {
+        console.warn('No Picture for item:', item);
+    }
+
+    const url = document.createElement('a');
+    url.href = item.properties.URL?.url || '#';
+    url.textContent = "Visit website";
+    brandElement.appendChild(url);
+
+    return brandElement;
 };
 
 window.onload = async () => {
     try {
         const data = await fetchNotionData();
-        const brandsContainer = document.getElementById('brands-container');
-
-        const groupElements = {}; // 存储不同分组的元素
+        const groups = {};
 
         for (const item of data.results) {
-            const brandElement = document.createElement('div');
-            brandElement.classList.add('brand-card');
+            const groupTitle = item.properties.Group?.select?.name;
 
-            const brandName = document.createElement('h2');
-            brandName.textContent = item.properties.Brand?.title[0]?.plain_text || 'Unknown';
-            brandElement.appendChild(brandName);
+            if (!groups[groupTitle]) {
+                groups[groupTitle] = document.createElement('div');
+                groups[groupTitle].classList.add('group');
 
-            const intro = document.createElement('p');
-            intro.textContent = item.properties.Introduction?.rich_text[0]?.plain_text || 'No Introduction';
-            brandElement.appendChild(intro);
-
-            const logoFile = item.properties.Logo?.files[0];
-            if (logoFile && logoFile.file) {
-                const logo = document.createElement('img');
-                logo.src = logoFile.file.url; 
-                brandElement.appendChild(logo);
-            } else {
-                console.warn('No Logo for item:', item);
+                const groupTitleElement = document.createElement('h2');
+                groupTitleElement.textContent = groupTitle;
+                groups[groupTitle].appendChild(groupTitleElement);
             }
 
-            const pictureFile = item.properties.Picture?.files[0];
-            if (pictureFile && pictureFile.file) {
-                const picture = document.createElement('img');
-                picture.src = pictureFile.file.url;
-                brandElement.appendChild(picture);
-            } else {
-                console.warn('No Picture for item:', item);
-            }
+            const brandElement = createBrandElement(item);
+            groups[groupTitle].appendChild(brandElement);
+        }
 
-            const url = document.createElement('a');
-            url.href = item.properties.URL?.url || '#';
-            url.textContent = "Visit website";
-            brandElement.appendChild(url);
-
-            const brandGroup = item.properties.Group?.select?.name;
-            if (brandGroup) {
-                if (!groupElements[brandGroup]) { // 如果这个分组的元素还没有创建
-                    // 创建一个新的元素来展示这个分组
-                    const groupElement = document.createElement('div');
-                    groupElement.id = brandGroup;
-                    groupElement.classList.add('brand-group');
-
-                    const groupTitle = document.createElement('h1');
-                    groupTitle.textContent = brandGroup;
-                    groupElement.appendChild(groupTitle);
-
-                    groupElements[brandGroup] = groupElement;
-                    brandsContainer.appendChild(groupElement);
-                }
-
-                // 将品牌添加到对应的分组中
-                groupElements[brandGroup].appendChild(brandElement);
-            } else {
-                console.warn('No Group for item:', item);
-            }
+        const brandsElement = document.getElementById('brands');
+        for (const group in groups) {
+            brandsElement.appendChild(groups[group]);
         }
     } catch (error) {
         console.error("Failed to fetch brand data:", error);
