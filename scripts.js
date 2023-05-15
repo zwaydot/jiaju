1df0884b51a74d951d286df964aedc18ff2562ae
zway <zwaydot@yeah.net>
2023-05-15 21:47:17 +0800

    解决提交冲突

--- scripts.js
+++ scripts.js
@@ -29,42 +29,56 @@ const createBrandElement = (item) => {
     intro.textContent = item.properties.Introduction?.rich_text[0]?.plain_text || 'No Introduction';
     brandElement.appendChild(intro);
 
-            const logoFile = item.properties.Logo?.files[0];
-            if (logoFile) {
-                const logo = document.createElement('img');
-                logo.src = logoFile.file.url; // 将logoFile.url更改为logoFile.file.url
-                brandElement.appendChild(logo);
-            } else {
-                console.warn('No Logo for item:', item);
-            }
-            
-            const pictureFile = item.properties.Picture?.files[0];
-            if (pictureFile) {
-                const picture = document.createElement('img');
-                picture.src = pictureFile.url;
-                brandElement.appendChild(picture);
-            } else {
-                console.warn('No Picture for item:', item);
-            }
+    const logoFile = item.properties.Logo?.files[0];
+    if (logoFile && logoFile.file) {
+        const logo = document.createElement('img');
+        logo.src = logoFile.file.url; 
+        brandElement.appendChild(logo);
+    } else {
+        console.warn('No Logo for item:', item);
+    }
 
+    const pictureFile = item.properties.Picture?.files[0];
+    if (pictureFile && pictureFile.file) {
+        const picture = document.createElement('img');
+        picture.src = pictureFile.file.url;
+        brandElement.appendChild(picture);
+    } else {
+        console.warn('No Picture for item:', item);
+    }
 
+    const url = document.createElement('a');
+    url.href = item.properties.URL?.url || '#';
+    url.textContent = "Visit website";
+    brandElement.appendChild(url);
 
-            // const logo = document.createElement('img');
-            // logo.src = item.properties.Logo?.files[0]?.url || 'No Logo';
-            // brandElement.appendChild(logo);
+    return brandElement;
+}
 
-            const url = document.createElement('a');
-            url.href = item.properties.URL?.url || '#';
-            url.textContent = "Visit website";
-            brandElement.appendChild(url);
-
-            // const picture = document.createElement('img');
-            // picture.src = item.properties.Picture?.files[0]?.url || 'No Picture';
-            // brandElement.appendChild(picture);
-
+window.onload = async () => {
+    try {
+        const data = await fetchNotionData();
         const brandsElement = document.getElementById('brands');
-        for (const group in groups) {
-            brandsElement.appendChild(groups[group]);
+        const groups = {};
+
+        for (const item of data.results) {
+            const groupTitle = item.properties.Group?.select?.name;
+
+            if (!groups[groupTitle]) {
+                groups[groupTitle] = document.createElement('div');
+                groups[groupTitle].classList.add('group');
+
+                const groupTitleElement = document.createElement('h2');
+                groupTitleElement.textContent = groupTitle;
+                groups[groupTitle].appendChild(groupTitleElement);
+            }
+
+            const brandElement = createBrandElement(item);
+            groups[groupTitle].appendChild(brandElement);
+        }
+
+        for (const groupTitle in groups) {
+            brandsElement.appendChild(groups[groupTitle]);
         }
     } catch (error) {
         console.error("Failed to fetch brand data:", error);
